2ef94e747538b9f21bed61d198e6d792924234d9
Aaron Garner <aaron.garner.1982@gmail.com>
2020-06-15 15:37:31 -0700

    updated the name in package.json and on index.html and update the mongoose.connect in server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds163680.mlab.com:63680/heroku_l0d01xzn");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Start the API server
 app.listen(PORT, function() {
