ac598bef080987fd5de216af5020d0d941d6a435
Aaron Garner <aaron.garner.1982@gmail.com>
2021-01-05 19:31:31 -0800

    updated production build after updating dependencies

---
+++ client/build/precache-manifest.7365a2581709df1394f5e014d56c63c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "468a5722723bc665d283",
+    "url": "/static/css/main.c6e2797f.chunk.css"
+  },
+  {
+    "revision": "468a5722723bc665d283",
+    "url": "/static/js/main.06cb78ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f281c1df45c4648023a",
+    "url": "/static/js/2.09d506a8.chunk.js"
+  },
+  {
+    "revision": "f5996213a41ddffc1e3bc5f8c16ba91b",
+    "url": "/index.html"
+  }
+];
